a1f00a6aae2a63bda1625258d387f19589431fae
mlu <melodylu@users.noreply.github.com>
2015-11-19 17:39:18 -0800

    [feat]: Edit express server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
 
 app.use(express.static(__dirname.slice(0, -6) + 'dist'));
