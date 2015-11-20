b36addcf161275f1f6852aee42e78f9c3db2c68c
Michael Ericson <ericson.michael.j@gmail.com>
2015-11-19 19:02:44 -0800

    Feat: Can now add images

--- build/webpack/_base.js
+++ build/webpack/_base.js
@@ -71,7 +71,8 @@ const webpackConfig = {
       { test: /\.woff2(\?.*)?$/, loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2" },
       { test: /\.ttf(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?.*)?$/,   loader: "file-loader?prefix=fonts/&name=[path][name].[ext]" },
-      { test: /\.svg(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml" }
+      { test: /\.svg(\?.*)?$/,   loader: "url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml" },
+      { test: /\.(png|jpg)$/,    loader: 'file-loader?name=images/[name].[ext]' }
       /* eslint-enable */
     ]
   },
