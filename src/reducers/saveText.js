fc1ba897856270a322b47340956234b5c24b6417
mlu <melodylu@users.noreply.github.com>
2015-11-20 11:29:55 -0800

    [feat]: Add React test components

---
+++ src/reducers/saveText.js
@@ -0,0 +1,31 @@
+import { createReducer }     from '../utils';
+import { SAVE_TEXT } from 'constants/saveText';
+
+const initialState = 'Test';
+export default createReducer(initialState, {
+  // so this isn't incrementing the counter; it's changing the state?
+  [SAVE_TEXT] : (state, payload) => {
+    console.log(state)
+    return state + 'fooo'
+  }
+});
+
+/*
+function createReducer (initialState, fnMap) {
+  return (state = initialState, { type, payload }) => {
+    const handler = fnMap[type];
+
+    return handler ? handler(state, payload) : state;
+  };
+}
+
+(state, { type, payload }) => {
+    const handler = SAVE_TEXT;
+
+    return  handler(state, payload);
+  };
+}
+
+
+
+*/
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,17 @@ import CoreLayout            from 'layouts/CoreLayout';
 import HomeView              from 'views/HomeView';
 import ResumeView            from 'views/ResumeView';
 import ResumeLayout          from 'layouts/ResumeLayout';
+import EditTextView          from 'views/editText';
+import EditTextLayout        from 'layouts/editText';
 
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
     <Route path='/resume' component={ResumeLayout}>
       <IndexRoute component={ResumeView} />
+    </Route>
+     <Route path='/editText' component={EditTextLayout}>
+      <IndexRoute component={EditTextView} />
     </Route>
   </Route>
 );
