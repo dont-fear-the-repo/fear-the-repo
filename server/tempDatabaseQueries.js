40f315a02f7a7ec13ef234b75f208c71d99644ae
Sujay Patel <sujay.sanjay.patel@gmail.com>
2015-11-21 14:41:57 -0800

    [feat]: In progress database queries

--- server/tempDatabaseQueries.js
+++ server/tempDatabaseQueries.js
@@ -1,79 +1,59 @@
-//Retrieve Information
+//QUERY : RETRIEVE A NEW BULLET FOR GIVEN USER->RESUME->BLOCKS-BULLETS
 
-app.get("/api/bullets"//this needs to be filled in with proper route
-  , function(req, res) {
-  User.findOne({
-    where: {
-      user_name: req.session.user.user_name
-    }
-  }).then(function(user) {
-    user.getResumes().then(function(resumes) {
-      resumes = _.map(collections, function(item) {
-        return item.collection;
-      });
-      res.send(collections);
-    });
-  });
-
-
-////Insert Information
-
-app.post("/api/collection", function(req, res) {
-  User.findOne({
-    where: {
-      user_name: req.session.user.user_name
-    }
-  }).then(function(user) {
-    var user_id = user.id;
-    Collection.findOne({
-      where: {
-        collection: req.body.collection,
-        user_id: user_id
-      }
-    }).then(function(collection) {
-      Book.create(req.body.book)
-        .then(function(book) {
-          collection.addBook(book);
-          res.status(201).send("succesfully added book");
-        });
-    });
-  });
-});
-
-
-//Detele Information
-
-app.post("/api/collection/delete", function(req, res) {
-  // NY Times bestsellers arent stored in the database, theyre an
-  // an API call (not stored in the DB). If you try to delete a book
-  // from the bestsellers collection, the server will crash. This
-  // if statement prevents that from happening.
-  if (req.body.collection === "bestsellers") {
-    console.log("Cant delete from bestsellers");
-    return;
-  }
-  User.findOne({
-    where: {
-      user_name: req.session.user.user_name
-    }
-  }).then(function(user) {
-    var user_id = user.id;
-    Collection.findOne({
-      where: {
-        collection: req.body.collection,
-        user_id: user_id
-      }
-    }).then(function(collection) {
-      collection.getBooks({
+app.post("?"
+    //***TODO ADD route*** HAVE TO USE POST OF SEND INFORMATION ABOUT USER INSIDE QUERY
+    ,
+    function(req, res) {
+      User.findOne({
         where: {
-          title: req.body.book.title
+          user_name: //***TODO ADD req.session.user.user_name***
         }
-      }).then(function(books) {
-        books[0].destroy().then(function() {
-          console.log("successfully deleted book");
-          res.send("deleted book");
-        })
+      }).then(function(user) {
+          var id_users = user.id;
+          Resume.findOne({
+            where: {
+              key: //***TODO ADD req.body.resume***
+                id_users: id_users
+            }
+          }).then(function(resume) {
+              resume.getBlocks().then(funciton(block) {
+                block.getBullets()
+              })
+              //TODO TIE IN HOW TO GET ALL BULLETS BACK OUT
+              //REPLICATE TO INSERT RECORDS
+              // Block.create( //***TODO ADD req.body.block*** )
+              //   .then(function(block) {
+              //     resume.addBlock(block);
+              //     res.status(201).send("succesfully added block");
+              //   });
+              // });
+          });
       });
-    });
-  });
-});
+
+///QUERY : INSERT A NEW BULLET FOR GIVEN USER->RESUME->BLOCKS-BULLETS
+
+///QUERY : INSERT A NEW BLOCK FOR RESUME
+app.post("?"
+    //***TODO ADD route*** HAVE TO USE POST OF SEND INFORMATION ABOUT USER INSIDE QUERY
+    ,
+    function(req, res) {
+      User.findOne({
+        where: {
+          user_name: //***TODO ADD req.session.user.user_name***
+        }
+      }).then(function(user) {
+          var id_users = user.id;
+          Resume.findOne({
+            where: {
+              key: //***TODO ADD req.body.resume***
+                id_users: id_users
+            }
+          }).then(function(resume) {
+              Block.create( //***TODO ADD req.body.block*** )
+                .then(function(block) {
+                  resume.addBlock(block);
+                  res.status(201).send("succesfully added block");
+                });
+              });
+          });
+      });
