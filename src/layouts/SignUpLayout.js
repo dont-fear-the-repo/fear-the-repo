008657ac10621af7002e24b468168c431f5cf285
Michael Ericson <ericson.michael.j@gmail.com>
2015-11-21 11:59:49 -0800

    [feat]: Working on Signup and Login views

---
+++ src/layouts/SignUpLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'styles/core.scss';
+
+export default class SignUpLayout extends React.Component {
+  static propTypes = {
+    children : React.PropTypes.element
+  }
+
+  render () {
+    return (
+      <div className='page-container'>
+        <div className='view-container'>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
