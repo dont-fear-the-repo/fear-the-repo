8e01285cc14cd67e338e9c114fa28bb44d6390af
Ryan James <ryanwilsonjames@gmail.com>
2015-11-23 13:19:42 -0800

    [feat]: Update doc title and favicon, code cleanup

--- src/actions/titleBarActions.js
+++ src/actions/titleBarActions.js
@@ -4,12 +4,12 @@ export function loginUser(payload) {
   return {
     type: LOGIN_USER,
     payload: payload
-  }
+  };
 }
 
 export function signupUser(payload) {
   return {
     type: SIGNUP_USER,
     payload: payload
-  }
+  };
 }
