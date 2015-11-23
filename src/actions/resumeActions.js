9d2e68b44b107b8b49c72686e78098194b08d4af
Sujay Patel <sujay.sanjay.patel@gmail.com>
2015-11-23 11:11:24 -0800

    [feat]: Work in progress save to dataase

--- src/actions/resumeActions.js
+++ src/actions/resumeActions.js
@@ -1,8 +1,38 @@
 import { SAVE_RESUME } from 'constants/resumeConstants';
+//import request from '../_config/superagent';
 
-export function saveResume (payload) {
+export function saveResumeRequest (payload) {
   return {
     type: SAVE_RESUME,
     payload: payload
   };
 }
+
+export function saveResumeDb(payload){
+  return dispatch => {
+    dispatch(saveResumeRequest(payload));
+      return console.log('in dbSaveResume');
+    }
+  //   return request
+  //     .post()
+  //     // ** TODO add url ** // ex. serverUrl + '/saveResume'
+  //     .send({
+  //       payload: payload
+  //     })
+  //     .end((err,res={}) => {
+  //       err ? dispatch(dbSaveResumeError(err))
+  //       : dispatch(dbSaveResumeSucess());
+  //     });
+  //   };
+}
+
+
+export function dbSaveResumeSucess(){
+  return console.log("woo you saved to the database!");
+}
+
+export function dbSaveResumeError(err){
+  return console.log("there is an error saving resume to db", err);
+}
+
+//create action 1. post 2. response
