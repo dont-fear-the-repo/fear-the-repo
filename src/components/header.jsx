46d5f017d4887d6a2298c0ac833e03d15744cb83
Michael Ericson <ericson.michael.j@gmail.com>
2015-11-23 19:50:16 -0500

    [chore]: ran eslint

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -140,37 +140,30 @@ export class Header extends React.Component {
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
 
 /***************************************************
--*******     Useful code for the future      ********
--
--
--// TO CREATE A POP-UP DIALOG (for Auth maybe?)
--let customActions = [
--  <FlatButton
--    label="Cancel"
--    secondary={true}
--    onTouchTap={this._handleCustomDialogCancel} />,
--  <FlatButton
--    label="Submit"
--    primary={true}
--    onTouchTap={this._handleCustomDialogSubmit} />
--];
--
--<Dialog
--  title="Dialog With Custom Actions"
--  actions={customActions}
--  open={this.state.showDialogCustomActions}
--  onRequestClose={this._handleRequestClose}>
--  The actions in this window were passed in as an array of react objects.
--</Dialog>
--
--
--// FOR RESPONSIVE DESIGN FOR MOBILE
--  // replace the header with a Toolbar from Material-ui
--
--// POTENTIALLY ALSO USEFUL:
--  // Dropdown Menu
--  // Popover (maybe instead of the dialog for auth?)
--  // Refresh Indicator
--  // Tabs?
--
--  *
+*******     Useful code for the future      ********
+// TO CREATE A POP-UP DIALOG (for Auth maybe?)
+let customActions = [
+  <FlatButton
+    label="Cancel"
+    secondary={true}
+    onTouchTap={this._handleCustomDialogCancel} />,
+  <FlatButton
+    label="Submit"
+    primary={true}
+    onTouchTap={this._handleCustomDialogSubmit} />
+];
+<Dialog
+  title="Dialog With Custom Actions"
+  actions={customActions}
+  open={this.state.showDialogCustomActions}
+  onRequestClose={this._handleRequestClose}>
+  The actions in this window were passed in as an array of react objects.
+</Dialog>
+// FOR RESPONSIVE DESIGN FOR MOBILE
+  // replace the header with a Toolbar from Material-ui
+// POTENTIALLY ALSO USEFUL:
+  // Dropdown Menu
+  // Popover (maybe instead of the dialog for auth?)
+  // Refresh Indicator
+  // Tabs?
+  */
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,36 +5,36 @@ import CoreLayout from 'layouts/CoreLayout';
 import HomeView from 'views/HomeView';
 import ResumeView from 'views/ResumeView';
 import UserFormView from 'views/UserFormView';
-import LoginView  from 'views/LoginView';
 import AboutView from 'views/AboutView';
 import SecretView from 'views/SecretView';
-import $		 from  'jQuery'; 
+import $ from 'jQuery';
 
-function requireAuth(nextState,replaceState){
+function requireAuth(nextState, replaceState) {
   //Note will change url address when deployed
-	$.ajax({
-      url: 'http://localhost:3000/authentication',
-      async: false,
-      type: 'POST',
-      contentType: 'application/json',
-      success: function(data){
-        if(data.Auth === false){
-         replaceState({ nextPathname: nextState.location.pathname }, '/login')
-        }
-      },
-      error: function(xhr,status,err){
-        console.log("I'm a failure")
-      }
-    });
-}
 
+  $.ajax({
+    url: 'http://localhost:3000/authentication',
+    async: false,
+    type: 'POST',
+    contentType: 'application/json',
+    success: function (data) {
+      if (data.Auth === false) {
+        replaceState({
+          nextPathname: nextState.location.pathname
+        }, '/login');
+      }
+    },
+    error: function (xhr, status, err) {
+      console.error(err);
+    }
+  });
+};
 
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
     <Route path='/userform' component={UserFormView} />
     <Route path='/resume' component={ResumeView} />
-    <Route path='/login' component={LoginView} />
     <Route path='/about' component={AboutView} />
     <Route path = '/secretpage' component={SecretView} onEnter={requireAuth} />
   </Route>
