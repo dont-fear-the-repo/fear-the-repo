541a83506fdf561c87aa830468cc6fcecb7f38af
Michael Ericson <ericson.michael.j@gmail.com>
2015-11-25 15:17:02 -0500

    [feat]: header now contains props

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -2,21 +2,155 @@ import React from 'react';
 import { Header } from 'components/header';
 import { Footer } from 'components/footer';
 import 'styles/core.scss';
+import { Link } from 'react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { loginUser, signupUser } from 'actions/titleBarActions';
+import $ from 'jQuery';
+import { FlatButton, Popover, TextField } from 'material-ui/lib';
+
+  const ActionCreators = {
+    loginUser: loginUser,
+    signupUser: signupUser
+  };
+
+  const mapStateToProps = (state) => ({
+    userLoginInfo: state.username,
+    Loggedin     : state.titleBarReducer.Loggedin
+  });
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(ActionCreators, dispatch)
+});
+
+
 
 export default class CoreLayout extends React.Component {
   static propTypes = {
     children : React.PropTypes.element
+  };
+
+
+  state = {
+    activePopover: '',
+    anchorEl: {},
+    loginOrSignup: ''
   }
+
+// AUTH METHODS
+  handleLogin() {
+    const userLoginInfo = {
+      username: this.refs.username.getValue(),
+      password: this.refs.password.getValue()
+    };
+
+    // jQuery defeat...not "the Redux Way"?
+    $.ajax({  // TODO: eliminate jQuery!
+      url: '/login',
+      type: 'POST',
+      data: JSON.stringify(userLoginInfo),
+      contentType: 'application/json',
+      success: function(data) {
+        }.bind(this),
+      error: function(xhr,status,err) {
+          console.log("error")
+        }.bind(this)
+    });
+
+
+    // this.props.actions.loginUser(userLoginInfo);  // TODO: make this work? Currently this component has no props, and so no actions are being bound and available
+    // TODO: change button to show userinfo, maybe redirect? Possible async concerns
+  }
+
+  handleSignup() {
+    const userSignupInfo = {
+      username: this.refs.username.getValue(),
+      password: this.refs.password.getValue()
+    };
+
+    // jQuery defeat...not "the Redux Way"?
+
+    this.props.actions.signupUser(userSignupInfo);  // TODO: make this work? Currently this component has no props, and so no actions are being bound and available
+    // TODO: change button to show userinfo, maybe redirect? Possible async concerns
+  }
+
+// POPOVER METHODS
+  showLoginPopover(key, e) {
+    this.setState({
+      activePopover: key,
+      anchorEl: e.currentTarget,
+      loginOrSignup: 'login'
+    });
+  }
+
+  showSignupPopover(key, e) {
+    this.setState({
+      activePopover: key,
+      anchorEl: e.currentTarget,
+      loginOrSignup: 'signup'
+    });
+  }
+
+  closePopover(key) {
+    if (this.state.activePopover !== key)
+      return
+    this.setState({
+      activePopover:'none',
+    });
+  }
+
+
+
 
   render () {
     return (
       <div className='page-container'>
         <div className='view-container'>
-        <Header />
-          {this.props.children}
+      <div>
+        <div className='header'>
+          <Link to='/' style={{marginLeft: '30px'}}>Fear the Repo</Link>
+          <Link to='/userform'>
+            <FlatButton label='User Info' />
+          </Link>
+          <Link to='/resume'>
+            <FlatButton label='Edit Resume' />
+          </Link>
+          <FlatButton label='export' />
+          {this.props.Loggedin ? <Link to='/secretpage'>
+            <FlatButton label = 'Secret Page' />
+          </Link>: ""}  
+          <FlatButton style={{float: 'right', marginRight: '30px'}}
+                      label='Login'
+                      onClick={this.showLoginPopover.bind(this, 'pop')} />
+          <FlatButton style={{float: 'right', marginRight: '10px'}}
+                      label='Signup'
+                      onClick={this.showSignupPopover.bind(this, 'pop')} />
+          </div>
+        <Popover className='signup-popover'
+                 open={this.state.activePopover === 'pop'}
+                 anchorEl={this.state.anchorEl}
+                 anchorOrigin={{horizontal: 'left', vertical: 'center'}}
+                 targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                 canAutoPosition={true}
+                 onRequestClose={this.closePopover.bind(this, 'pop')} >
+          <div style={{padding: 20}}>
+            <TextField hintText='Username' ref='username' />
+            <TextField hintText='Password' type="password"  ref='password' />
+            <FlatButton label='Submit'
+                        onClick={this.state.loginOrSignup === 'login' ?
+                          e => this.handleLogin(e) :
+                          e => this.handleSignup(e)} />
+          </div>
+        </Popover>
+
+      </div>
+            {this.props.children}
         <Footer />
         </div>
       </div>
     );
   }
 }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoreLayout);
