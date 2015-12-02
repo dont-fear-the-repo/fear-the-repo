4c13d376f9899e1bd1cd0c18507d97b56ce5740d
Ryan James <ryanwilsonjames@gmail.com>
2015-12-01 16:10:58 -0800

    [style]: Add file for custom Material-UI theme (to be implemented later)

---
+++ src/styles/customtheme.js
@@ -0,0 +1,23 @@
+import mui from 'mui';
+import Colors from mui.Styles.Colors;
+import ColorManipulator from '../../node_modules/material-ui/lib/utils/color-manipulator';
+import Spacing from '../../node_modules/material-ui/lib/styles/spacing';
+
+
+module.exports = {
+  spacing: Spacing,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: Colors.cyan500,
+    primary2Color: Colors.cyan700,
+    primary3Color: Colors.grey400,
+    accent1Color: Colors.pinkA200,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500,
+    textColor: Colors.darkBlack,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    borderColor: Colors.grey300,
+    disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
+  }
+};
