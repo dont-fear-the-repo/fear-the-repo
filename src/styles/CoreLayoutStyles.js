70a6c0f5778cbf1a51dc18297ac950c7fa93fb71
Ryan James <ryanwilsonjames@gmail.com>
2015-12-04 11:26:21 -0800

    [refactor]: Update styling throughout entire app

--- src/styles/CoreLayoutStyles.js
+++ src/styles/CoreLayoutStyles.js
@@ -1,24 +1,52 @@
+import { MasterTheme } from 'styles/MasterTheme';
+
+const font = MasterTheme.muli;
+const fontsize = '14pt';
+const buttonTopMargin = '8px';
+const errorTextMargin = '20px 0 0 30px';
+
 export const styles = {
+  mainContainer: {
+    backgroundColor: MasterTheme.darkGray
+  },
+  buttonColor: MasterTheme.white,
+  buttonHoverColor: MasterTheme.orange,
   name: {
+    fontFamily: font,
+    fontSize: '20pt',
+    color: MasterTheme.orange,
     marginLeft: '30px',
-    marginRight: '20px'
+    marginRight: '20px',
+    marginTop: '12px'
+  },
+  resumeButton: {
+    fontFamily: font,
+    fontSize: fontsize,
+    marginTop: buttonTopMargin,
+    marginBottom: buttonTopMargin
   },
   loginButton: {
     float: 'right',
-    marginRight: '30px'
+    marginRight: '30px',
+    marginTop: buttonTopMargin,
+    marginBottom: buttonTopMargin,
+    fontSize: fontsize,
+    fontFamily: font
   },
   signupButton: {
     float: 'right',
-    marginRight: '10px'
+    marginRight: '10px',
+    marginTop: buttonTopMargin,
+    marginBottom: buttonTopMargin,
+    fontSize: fontsize,
+    fontFamily: font
   },
   errorText: {
-    marginTop: '20px',
-    marginLeft: '30px',
-    color: 'red'
+    margin: errorTextMargin,
+    color: MasterTheme.orange
   },
   disabledText: {
-    marginTop: '20px',
-    marginLeft: '30px',
-    color: 'blue'
+    margin: errorTextMargin,
+    color: MasterTheme.midGray
   }
 };
