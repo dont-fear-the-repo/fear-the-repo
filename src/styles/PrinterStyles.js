2b1dd0a4621588d7a677161c674b655ba0585c93
Michael Ericson <ericson.michael.j@gmail.com>
2015-12-07 16:08:26 -0800

    [styling]: Styled the printer when printing

---
+++ src/styles/PrinterStyles.js
@@ -0,0 +1,11 @@
+export const printStyles =
+`<style>
+	div {
+ border-radius: 0px !important;
+ box-shadow: none !important; }
+
+	@media print {
+	  @page { margin: 0; }
+	  body { margin: 1.6cm; }
+	}
+</style>`;
