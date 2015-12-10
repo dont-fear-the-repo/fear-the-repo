9dc709e60536d8f9a36533fd46571bac1c00b1a8
Michael Ericson <ericson.michael.j@gmail.com>
2015-12-09 16:07:16 -0800

    [Refactor]: Can now compile

--- bin/compile.js
+++ bin/compile.js
@@ -1,4 +1,35 @@
 require('babel/register');
 
-const config   = require('./config');
-module.exports = require('./build/webpack/' + config.get('env'));
+const config   = require('../config');
+const debug    = require('debug')('kit:bin:compile');
+console.log(config)
+debug('Create webpack compiler.');
+
+const compiler = require('webpack')(
+  require('../build/webpack/' + config.env)
+);
+
+compiler.run(function (err, stats) {
+  const jsonStats = stats.toJson();
+
+  debug('Webpack compile completed.');
+  console.log(stats.toString({
+    chunks : false,
+    chunkModules : false,
+    colors : true
+  }));
+
+  if (err) {
+    debug('Webpack compiler encountered a fatal error.', err);
+    process.exit(1);
+  } else if (jsonStats.errors.length > 0) {
+    debug('Webpack compiler encountered errors.');
+    process.exit(1);
+  } else if (jsonStats.warnings.length > 0) {
+    debug('Webpack compiler encountered warnings.');
+
+    if (config.compiler_fail_on_warning) {
+      process.exit(1);
+    }
+  }
+});
