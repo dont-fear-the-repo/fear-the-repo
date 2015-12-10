c46cb7b21967a4547d475de6eff12a54973d26c9
Michael Ericson <ericson.michael.j@gmail.com>
2015-12-09 17:13:50 -0800

    [bug]: Fix bullet issues

--- src/components/BlockDumbComp.js
+++ src/components/BlockDumbComp.js
@@ -163,7 +163,7 @@ export default class BlockDumbComp extends React.Component {
 
             {Radium.getState(this.state, 'block', ':hover') ? (
 
-              <img src={require('styles/assets/ic_remove_circle_outline_black_24px.svg')}
+              <img src=require('styles/assets/ic_remove_circle_outline_black_24px.svg')
                      onClick={e => this.hideBlock(e, this.props.blockId)} />
                 ) : null}
 
@@ -172,7 +172,7 @@ export default class BlockDumbComp extends React.Component {
               </div>
 
             {Radium.getState(this.state, 'block', ':hover') ? (
-            <img src={require('styles/assets/ic_add_circle_outline_black_24px.svg')}
+            <img src=require('styles/assets/ic_add_circle_outline_black_24px.svg')
                 onClick={e => this.addBullet(e, this.props.blockId)} />
               ) : null}
           </div>
--- src/components/Bullet.js
+++ src/components/Bullet.js
@@ -127,12 +127,12 @@ export default class Bullet extends React.Component {
 
         {Radium.getState(this.state, 'bullet', ':hover') ? (
 
-        <img src={require('styles/assets/ic_remove_circle_outline_black_24px.svg')}
+        <img src='styles/assets/ic_remove_circle_outline_black_24px.svg'
              onClick={e => this.hideBullet(e, this.props.bulletId)} />
             ) : null}
         
         {Radium.getState(this.state, 'bullet', ':hover') ? (
-        <img src={require('styles/assets/drag-vertical.png')} style={styles.handle} />
+        <img src='styles/assets/drag-vertical.png' style={styles.handle} />
           ) : null}
 
       </div>
