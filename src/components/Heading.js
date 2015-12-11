620a3ce6298b85ae46fdbf4e0cfc159cdb2e6512
Ryan James <ryanwilsonjames@gmail.com>
2015-12-11 15:12:27 -0800

    [fix]: Heading blocks can be archived again

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -112,16 +112,20 @@ export default class Heading extends React.Component {
     return connectDragSource(connectDropTarget(
       <div style={blockDrag} key='heading'>
 
-        {Radium.getState(this.state, 'heading', ':hover')}
-          <Editor style={resumeThemes[currentTheme].headingTitle}
-                  text={this.props.companyName}
-                  options={{toolbar: false}}
-                  onBlur={e => this.props.handleUpdateLocalState(e, 'companyName', 'blocks', this.props.blockId)} />
+        <Editor style={resumeThemes[currentTheme].headingTitle}
+                text={this.props.companyName}
+                options={{toolbar: false}}
+                onBlur={e => this.props.handleUpdateLocalState(e, 'companyName', 'blocks', this.props.blockId)} />
 
-          <Editor style={resumeThemes[currentTheme].location}
+        <Editor style={resumeThemes[currentTheme].location}
                   text={this.props.location}
                   options={{toolbar: false}}
                   onBlur={e => this.props.handleUpdateLocalState(e, 'location', 'blocks', this.props.blockId)} />
+
+        {Radium.getState(this.state, 'heading', ':hover') ? (
+          <img src={require('styles/assets/ic_remove_circle_outline_black_24px.svg')}
+                 onClick={e => this.hideBlock(e, this.props.blockId)} />
+            ) : null}
 
       </div>
     ));
