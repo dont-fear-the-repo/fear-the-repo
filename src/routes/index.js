66a33877ec95b62d09927e477f566b6052ed0d50
Ryan James <ryanwilsonjames@gmail.com>
2015-12-12 15:40:28 -0800

    [refactor]: Code cleanup: reducers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import $ from 'jquery';
 
 import CoreLayout from 'layouts/CoreLayout';
 import HomeView from 'views/HomeView';
 import ResumeView from 'views/ResumeView';
 import AboutView from 'views/AboutView';
-import SecretView from 'views/SecretView';
-import LinkedinLoginView from 'views/LinkedInLoginView'
+import LinkedinLoginView from 'views/LinkedInLoginView';
 
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
     <Route path='/resume' component={ResumeView} />
     <Route path='/about' component={AboutView} />
-    <Route path = '/cookies' component={LinkedinLoginView} />
+    <Route path='/cookies' component={LinkedinLoginView} />
   </Route>
 );
