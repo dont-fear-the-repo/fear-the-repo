adb12ea094d1e4b82d45489ff3bccf84b5a7791f
Michael Ericson <ericson.michael.j@gmail.com>
2015-12-14 16:25:26 -0800

    [styling]: Fix pdf image appearing and borders appering

--- src/styles/PrinterStyles.js
+++ src/styles/PrinterStyles.js
@@ -1,13 +1,15 @@
 export const printStyles =
 `<style>
-	div {
- border-radius: 0px !important;
- box-shadow: none !important; }
+div {
+	border-width: 0px ! important;		
+ 	border-radius: 0px !important;
+ 	box-shadow: none !important;}
 
-	@media print {
-	  @page { margin: 0; }
-	  body { margin: 0 0; }
-	  img {visibility: hidden; }
-	}
+ img {
+ 	visibility: hidden; }
+@media print {
+	@page { margin: 0; }
+	 body { margin: 0 0; }
+}
 
 </style>`;
